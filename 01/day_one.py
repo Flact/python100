eacadc005b00b7df0eeab46c05e421987c36f065
Anushka Milinda <y5hech@pm.me>
2020-12-04 01:44:21 -0500

    Change folder name and complete the day 01

--- 01/day_one.py
+++
@@ -1,3 +0,0 @@
-print("Hello world\nHello Andrea")
-print("Hello world\nHello Andrea")
-print("print('this is a Word')")
---
+++ Codes/day_one.py
@@ -0,0 +1,32 @@
+# print("Hello world\nHello Andrea")
+# print("Hello world\nHello Andrea")
+# print("print('this is a Word')")
+
+# input('What is your name? ')
+# print("Hello " + input('What is your name? ') + "!")
+
+# len() function is an inbuilt function in Python that returns the length of the string.
+# print(len(input('What is your name? ')))
+
+
+# Switch given numbers and print those
+# a = input("a: ")
+# b = input("b: ")
+
+# c = a
+# a = b
+# b = c
+
+# print("a: " + a)
+# print("b : " + b)
+
+# ***** The Code Challange Answer *****
+# in few lines
+# print("#--Welcome to brand name generator--##")
+# print("\nYour Brand name is " + input('Where you grew up?\n') + " " + input('What is your pet name?\n'))
+
+# Each line break down
+print("#--Welcome to brand name generator--##")
+city = input('Where you grew up?\n')
+pet = input('What is your pet name?\n')
+print("\nYour Brand name is " + city + " " + pet)
