59c04be85dbc9447f8d0719677b51ee9c826d384
Anushka Milinda <y5hech@pm.me>
2020-12-05 17:42:51 -0500

    rolllercoaster

--- Codes/day_03.py
+++ Codes/day_03.py
@@ -1,3 +1,8 @@
 
-print("Welcome to the rollercoster")
+print("Welcome to the rollercoaster")
 height = int(input("Enter your height in cm: "))
+
+if height >= 120:
+    print("\nYou can ride rolllercoaster")
+else:
+    print("\nSorry, you have to graw taller")
