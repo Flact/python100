1ec2d8a2c11ebe4b3049790796eb27f4c5155e11
Anushka Milinda <y5hech@pm.me>
2020-12-05 18:54:25 -0500

    Leap year

--- Codes/day_03.py
+++ Codes/day_03.py
@@ -23,19 +23,36 @@
 
 # Rollercoaster with differnt pricese for differnt ages (elif)
 
-print("Welcome to the rollercoaster")
-height = int(input("Enter your height in cm: "))
+# print("Welcome to the rollercoaster")
+# height = int(input("Enter your height in cm: "))
 
-if height >= 120:
-    print("\nYou can ride rolllercoaster")
-    age  = int(input("Enter your age: "))
-    if age <= 12:
-        print("Please pay $5.")
-    elif age <= 18:
-        print("Please pay $7")
+# if height >= 120:
+#     print("\nYou can ride rolllercoaster")
+#     age  = int(input("Enter your age: "))
+#     if age <= 12:
+#         print("Please pay $5.")
+#     elif age <= 18:
+#         print("Please pay $7")
+#     else:
+#         print("Please pay $12")
+# else:
+#     print("\nSorry, you have to graw taller")
+
+# -----------------------------------------------------
+
+# Leap year
+
+year = int(input("Which year do you want to check? "))
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("this is a Leap Year")
+        else:
+            print("this is not a Leap Year")
     else:
-        print("Please pay $12")
+        print("this is a Leap Year")
 else:
-    print("\nSorry, you have to graw taller")
+    print("this is not a Leap Year")
 
 
