591da44d2a2a9cccf26b20f19bf47895d9f1d71a
Anushka Milinda <y5hech@pm.me>
2020-12-05 18:00:58 -0500

    rollercoaster (elif)

--- Codes/day_03.py
+++ Codes/day_03.py
@@ -1,8 +1,41 @@
 
+# if else condition
+
+# print("Welcome to the rollercoaster")
+# height = int(input("Enter your height in cm: "))
+
+# if height >= 120:
+#     print("\nYou can ride rolllercoaster")
+# else:
+#     print("\nSorry, you have to graw taller")
+
+# --------------------------------------------------------
+
+# odd even numbers
+
+# num = int(input("Enter decimal number: "))
+# if (num % 2 == 0):
+#     print("this number is an Even")
+# else:
+#     print("this number is an Odd")
+
+# ----------------------------------------------
+
+# Rollercoaster with differnt pricese for differnt ages (elif)
+
 print("Welcome to the rollercoaster")
 height = int(input("Enter your height in cm: "))
 
 if height >= 120:
     print("\nYou can ride rolllercoaster")
+    age  = int(input("Enter your age: "))
+    if age <= 12:
+        print("Please pay $5.")
+    elif age <= 18:
+        print("Please pay $7")
+    else:
+        print("Please pay $12")
 else:
     print("\nSorry, you have to graw taller")
+
+
