7dc2dcffe85d8e7ef8cc65fd45b53c57b26d19f5
Anushka Milinda <y5hech@pm.me>
2020-12-05 19:13:20 -0500

    Rollercoaster with photo (pay extra $3)

--- Codes/day_03.py
+++ Codes/day_03.py
@@ -42,17 +42,50 @@
 
 # Leap year
 
-year = int(input("Which year do you want to check? "))
+# year = int(input("Which year do you want to check? "))
 
-if year % 4 == 0:
-    if year % 100 == 0:
-        if year % 400 == 0:
-            print("this is a Leap Year")
-        else:
-            print("this is not a Leap Year")
-    else:
-        print("this is a Leap Year")
-else:
-    print("this is not a Leap Year")
+# if year % 4 == 0:
+#     if year % 100 == 0:
+#         if year % 400 == 0:
+#             print("this is a Leap Year")
+#         else:
+#             print("this is not a Leap Year")
+#     else:
+#         print("this is a Leap Year")
+# else:
+#     print("this is not a Leap Year")
+
+
+# --------------------------------------------------------
+
+# Rollercoaster photo
+
+
+# print("Welcome to the rollercoaster")
+# height = int(input("Enter your height in cm: "))
+
+# bill = 0
+
+# if height >= 120:
+#     print("\nYou can ride rolllercoaster")
+#     age  = int(input("Enter your age: "))
+#     if age <= 12:
+#         bill = 5
+#         print("Please pay for Child ticket $5.")
+#     elif age <= 18:
+#         bill = 7
+#         print("Please pay for Youth ticket $7")
+#     else:
+#         bill = 12
+#         print("Please pay for Adult ticket $12")
+    
+#     photo = input("\nDo you want a photo \"Y\" or \"N\" ")
+#     if photo == "Y":
+#         # add $3 to their bill
+#         bill += 3
+#     print(f"Please pay ${bill}")
+    
+# else:
+#     print("\nSorry, you have to graw taller")
 
 
