898a67681d324b9dd415684043ca84f7addf71d9
Anushka Milinda <y5hech@pm.me>
2020-12-05 21:18:56 -0500

    Completed Day 03

--- Codes/day_03.py
+++ Codes/day_03.py
@@ -98,7 +98,7 @@
 
 # total = 0
 
-# # Size of pizza 
+# # Size of pizza
 # if size == "S":
 #     total = 15
 # elif size == "M":
@@ -123,32 +123,90 @@
 
 # Middle life crisis (Age 45 - 55)
 
-print("Welcome to the rollercoaster")
-height = int(input("Enter your height in cm: "))
+# print("Welcome to the rollercoaster")
+# height = int(input("Enter your height in cm: "))
 
-bill = 0
+# bill = 0
 
-if height >= 120:
-    print("\nYou can ride rolllercoaster")
-    age  = int(input("Enter your age: "))
-    if age <= 12:
-        bill = 5
-        print("Please pay for Child ticket $5.")
-    elif age <= 18:
-        bill = 7
-        print("Please pay for Youth ticket $7")
-    elif age >= 45 and age <= 55:
-        print("Have a free ride on us. Thank You!")
-    else:
-        bill = 12
-        print("Please pay for Adult ticket $12")
+# if height >= 120:
+#     print("\nYou can ride rolllercoaster")
+#     age  = int(input("Enter your age: "))
+#     if age <= 12:
+#         bill = 5
+#         print("Please pay for Child ticket $5.")
+#     elif age <= 18:
+#         bill = 7
+#         print("Please pay for Youth ticket $7")
+#     elif age >= 45 and age <= 55:
+#         print("Have a free ride on us. Thank You!")
+#     else:
+#         bill = 12
+#         print("Please pay for Adult ticket $12")
 
-    photo = input("\nDo you want a photo \"Y\" or \"N\" ")
-    if photo == "Y":
-        # add $3 to their bill
-        bill += 3
-    print(f"Please pay ${bill}")
+#     photo = input("\nDo you want a photo \"Y\" or \"N\" ")
+#     if photo == "Y":
+#         # add $3 to their bill
+#         bill += 3
+#     print(f"Please pay ${bill}")
 
-else:
-    print("\nSorry, you have to graw taller")
+# else:
+#     print("\nSorry, you have to graw taller")
 
+# ------------------------------------------------------------------------
+
+
+# LOVE CALCULATOR
+
+# print("Welcome to the Love Calculator!")
+# name1 = input("What is your name? \n")
+# name2 = input("What is their name? \n")
+
+# combined_name = name1 + name2
+# lover_names = combined_name.lower()
+
+# t = lover_names.count("t")
+# r = lover_names.count("r")
+# u = lover_names.count("u")
+# e = lover_names.count("e")
+# l = lover_names.count("l")
+# o = lover_names.count("o")
+# v = lover_names.count("v")
+
+# score = int(str(t + r + u + e) + str(l + o + v + e))
+
+# if score <= 10 or score >= 90:
+#     print(f"Your score is {score}, you go together like coke and mentos.")
+# elif score >= 40 and score <= 50:
+#     print(f"Your score is {score}, you are alright together.")
+# else:
+#     print(f"Your score is {score}.")
+
+# -----------------------------------------------------------------------
+
+print(''' q!)*(&?';o[}KHB()q!)*(&?';o[}KHB()q!)*(&?';o[}KHB()q!)*(&?';o[}KHB()q!)*(&
+tyer(*^%HG68%bkHltyer(*^%HG68%bkHltyer(*^%HG68%bkHltyer(*^%HG68%bkHltyer(*
+12?!q3y*@  _._]sf12?!q3y*@  _._]sf12?!q3y*@ _._ ]sf12?!q3y*@ _._ ]sf12?!q3
+x!~p;l  ,a8P8Y8a,x!~p;l  ,a8P8Y8a,x!~p;l ,a8P8Y8a, x!~p;l ,a8P8Y8a, x!~p;l
+b, ff ,d,  .-.  ,b, ff ,d,  .-.  ,b, ff,d, .-.   ,b,  ff,d, .-.   ,b,  ff,
+d"b  d"b  d'I'b  d"b  d"b  d'I'b  d"b d"b d'I'b   d"b  d"b d'I'b   d"b  d"
+   ,D,   ,P 8 Y,    ,D,   ,P 8 Y,   ,D   ,P 8 Y,    ,D    ,P 8 Y,    ,D
+  ,P P, "d' 8 'b"  ,P P, "d' 8 'b" ,P P,"d' 8 'b"  ,P P ,"d' 8 'b"  ,P P ,
+ ,P   P,8gcg8gcg8 ,P   P,8gcg8gcg8,P   ,8gcg8gcg8, P   , 8gcg8gcg8, P   , 
+,8P"""8P,8P"""8P ,8P"""8P,8P"""8P,8P""8P,8P"""8P,8P"""8P, 8P"""8P,8P"""8P,
+d ,a8a, d /\ /\  d ,a8a, d /\ /\ d ,aa, d /\ /\ d ,a8a, d  /\ /\ d ,a8a, d
+8,P" "Y,8  ( )   8,P" "Y,8  ( )  8,P""Y,8  ( )  8,P" "Y,8   ( )  8,P" "Y,8
+8P'   `Y8.( o ). 8P'   `Y8.( o ).8P'  `Y8.( o ).8P'   `Y8 .( o ).8P'   `Y8
+8'/\ /\'8,  _  , 8'/\ /\'8,  _  ,8'/\/\'8,  _  ,8'/\ /\'8 ,  _  ,8'/\ /\'8
+8  ( )  8;.(_).; 8  ( )  8;.(_).;8  ()  8;.(_).;8  ( )  8 :.(_).;8  ( )  8
+8.( o ).8Ya   aP 8.( o ).8Ya   aP8.(  ).8Ya   aP8.( o ).8 Ya   aP8.( o ).8
+8=-=-=-=8 "YaP"  8=-=-=-=8 "YaP" 8=-==-=8 "YaP" 8=-=-=-=8  "YaP" 8=-=-=-=8
+Y"""a"""Yaa,,,aa Y"""a"""Yaa,,,aaY""""""Yaa,,,aaY"""a"""Y aa,,,aaY"""a"""Y
+ `bag,d'  ``""''  `bag,d'  ``""'' `bagd'  ``""'' `b agd'   ``""'' `b agd' 
+  `YPY'   ,aaa,    `YPY'   ,aaa,   `YPY   ,aaa,    `YPY'   ,aaa,    `YPY'  
+  P "Y  Ya_).(_aY  P "Y  Ya_).(_aY  P"Y  Y_).(_ aY  P" Y  Y_).(_ aY  P" Y 
+"' de `"Y_"" ""_P"' de `"Y_"" ""_P"' de`"Y_"" ""_P"'  de`"Y_"" ""_P"'  de`"
+ swed @# sf;lks   swed @# sf;lks   swed @# sf;lks   swed @# sf;lks   swed @
+fkl*  mproot@mtuzfkl*  mproot@mtuzfkl*  mproot@mtuzfkl* mproot@mtu zfkl* mp
+.;uiyhkg&6uiOP98p.;uiyhkg&6uiOP98p.;uiyhkg&6uiOP98p.;uiyhkg&6uiOP98p.;uiyhk
+       
+ ''')
