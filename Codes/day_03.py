e8412b164761fa99eab184515498a9ff50cb79c1
Anushka Milinda <y5hech@pm.me>
2020-12-05 20:21:00 -0500

    Middle life crisis (Age 45 - 55)

--- Codes/day_03.py
+++ Codes/day_03.py
@@ -91,30 +91,64 @@
 
 # Automatic pizza order program
 
-print("\nWelcome to Python Pizza Deliveries!")
-size = input("\nWhat size pizza do you want? S, M, or L ")
-add_pepperoni = input("\nDo you want pepperoni? Y or N ")
-extra_cheese = input("\nDo you want extra cheese? Y or N ")
+# print("\nWelcome to Python Pizza Deliveries!")
+# size = input("\nWhat size pizza do you want? S, M, or L ")
+# add_pepperoni = input("\nDo you want pepperoni? Y or N ")
+# extra_cheese = input("\nDo you want extra cheese? Y or N ")
 
-total = 0
+# total = 0
 
-# Size of pizza 
-if size == "S":
-    total = 15
-elif size == "M":
-    total = 20
-else:
-    total = 25
+# # Size of pizza 
+# if size == "S":
+#     total = 15
+# elif size == "M":
+#     total = 20
+# else:
+#     total = 25
 
-# add pepperoni
-if add_pepperoni == "Y":
-    if size == "S":
-        total += 2
+# # add pepperoni
+# if add_pepperoni == "Y":
+#     if size == "S":
+#         total += 2
+#     else:
+#         total += 3
+
+# # extra cheese
+# if extra_cheese == "Y":
+#     total += 1
+
+# print(f"Your final bill is: ${total}.")
+
+# -------------------------------------------------------
+
+# Middle life crisis (Age 45 - 55)
+
+print("Welcome to the rollercoaster")
+height = int(input("Enter your height in cm: "))
+
+bill = 0
+
+if height >= 120:
+    print("\nYou can ride rolllercoaster")
+    age  = int(input("Enter your age: "))
+    if age <= 12:
+        bill = 5
+        print("Please pay for Child ticket $5.")
+    elif age <= 18:
+        bill = 7
+        print("Please pay for Youth ticket $7")
+    elif age >= 45 and age <= 55:
+        print("Have a free ride on us. Thank You!")
     else:
-        total += 3
+        bill = 12
+        print("Please pay for Adult ticket $12")
 
-# extra cheese
-if extra_cheese == "Y":
-    total += 1
+    photo = input("\nDo you want a photo \"Y\" or \"N\" ")
+    if photo == "Y":
+        # add $3 to their bill
+        bill += 3
+    print(f"Please pay ${bill}")
 
-print(f"Your final bill is: ${total}.")
+else:
+    print("\nSorry, you have to graw taller")
+
