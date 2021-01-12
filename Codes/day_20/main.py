bccfba5b6cc2f30080fe49cb2d30617d8ca15c4d
Anushka Milinda <anushkamilinda9@gmail.com>
2021-01-12 12:09:13 -0500

    # Detect collision with wall # Detect collision with tail # Slicing the segments of the snake(main.py) # Add segment using add_segment()

--- Codes/day_20/main.py
+++ Codes/day_20/main.py
@@ -2,6 +2,7 @@ from turtle import Screen
 from snake import Snake
 from time import sleep
 from food import Food
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -11,6 +12,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -27,5 +29,18 @@ while is_game_on:
 #     detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
+        scoreboard.increase_score()
+        snake.extend()
+
+    # detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        is_game_on = False
+        scoreboard.game_over()
+
+    # detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            is_game_on = False
+            scoreboard.game_over()
 
 screen.exitonclick()
---
+++ Codes/day_20/scoreboard.py
@@ -0,0 +1,28 @@
+from turtle import Turtle
+
+ALIGNMENT = "center"
+FONT = ("PottaOne-Regular", 18, "normal")
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, 270)
+        self.update_score()
+        self.hideturtle()
+        # self.clear()
+
+    def update_score(self):
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_score()
