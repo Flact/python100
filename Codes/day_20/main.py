951f6a92a46cd00578c922108bdf6f373b105eaa
Anushka Milinda <anushkamilinda9@gmail.com>
2021-01-12 01:20:12 -0500

    Keypress validation(make can not go to opposite direction)

--- Codes/day_20/main.py
+++ Codes/day_20/main.py
@@ -10,6 +10,12 @@ screen.tracer(0)
 
 snake = Snake()
 
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
 is_game_on = True
 while is_game_on:
     screen.update()
