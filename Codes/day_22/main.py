a6efcc246d6492ef0e3d905d04755ea86e6efb27
Anushka Milinda <anushkamilinda9@gmail.com>
2021-01-13 12:33:21 -0500

    Write the Ball Class and Make the Ball Move

--- Codes/day_22/main.py
+++ Codes/day_22/main.py
@@ -1,5 +1,7 @@
 from turtle import Screen, Turtle
+from time import sleep
 from paddle import Paddle
+from ball import Ball
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -11,6 +13,8 @@ screen.tracer(0)
 l_paddle = Paddle((-350, 0))
 r_paddle = Paddle((350, 0))
 
+ball = Ball()
+
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
 screen.onkey(r_paddle.go_down, "Down")
@@ -19,6 +23,8 @@ screen.onkey(l_paddle.go_down, "s")
 
 is_game_on = True
 while is_game_on:
+    sleep(0.1)
     screen.update()
+    ball.move()
 
 screen.exitonclick()
