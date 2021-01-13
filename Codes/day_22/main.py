4bb7d4dddac52981ed98c377a04d475c12f1b88b
Anushka Milinda <anushkamilinda9@gmail.com>
2021-01-13 12:16:01 -0500

    Created the paddle class and add functionalities to move up and down, (#Screen Update)

---
+++ Codes/day_22/main.py
@@ -0,0 +1,24 @@
+from turtle import Screen, Turtle
+from paddle import Paddle
+
+screen = Screen()
+screen.setup(width=800, height=600)
+# screen.bgpic("image.png")
+screen.bgcolor("black")
+screen.title("PONG")
+screen.tracer(0)
+
+l_paddle = Paddle((-350, 0))
+r_paddle = Paddle((350, 0))
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+is_game_on = True
+while is_game_on:
+    screen.update()
+
+screen.exitonclick()
