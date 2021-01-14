cac44e5355a425e1a53de76d4f1b4bdbd14008c1
Anushka Milinda <anushkamilinda9@gmail.com>
2021-01-14 01:17:22 -0500

    Detect collision with wall

--- Codes/day_22/main.py
+++ Codes/day_22/main.py
@@ -27,4 +27,9 @@ while is_game_on:
     screen.update()
     ball.move()
 
+    # Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce()
+
+
 screen.exitonclick()
