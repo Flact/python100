dabd949e360afcf94bb2319689b506d25bad362c
Anushka Milinda <anushkamilinda9@gmail.com>
2021-03-06 00:00:25 -0500

    Exception Handling in the NATO Phonetic Alphabet Project

--- nato-new-wtih-try-ex/main.py
+++ nato-new-wtih-try-ex/main.py
@@ -1,24 +1,24 @@
-student_dict = {
-    "student": ["Angela", "James", "Lily"],
-    "score": [56, 76, 98]
-}
-
-# Looping through dictionaries:
-for (key, value) in student_dict.items():
-    # Access key and value
-    # print(key, value)
-    pass
+# student_dict = {
+#     "student": ["Angela", "James", "Lily"],
+#     "score": [56, 76, 98]
+# }
+#
+# # Looping through dictionaries:
+# for (key, value) in student_dict.items():
+#     # Access key and value
+#     # print(key, value)
+#     pass
 
 import pandas
 
-student_data_frame = pandas.DataFrame(student_dict)
-
-# Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    # Access index and row
-    # print(row)
-    # Access row.student or row.score
-    pass
+# student_data_frame = pandas.DataFrame(student_dict)
+#
+# # Loop through rows of a data frame
+# for (index, row) in student_data_frame.iterrows():
+#     # Access index and row
+#     # print(row)
+#     # Access row.student or row.score
+#     pass
 
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
@@ -30,7 +30,19 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 phonetic_dict = {row["letter"]: row["code"] for (index, row) in data.iterrows()}
 print(phonetic_dict)
 
+
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-word = input("Enter a word: ").upper()
-output_list = [phonetic_dict[w] for w in word]
-print(output_list)
+
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    # print(type(word))
+    try:
+        output_list = [phonetic_dict[l] for l in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+
+generate_phonetic()
