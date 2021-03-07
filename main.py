5a208e7ea0c67ce1d0a3c7372dc313c272063dd7
Anushka Milinda <anushkamilinda9@gmail.com>
2021-03-07 01:50:39 -0500

    Creating the UI

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
-
+from tkinter import *
 
 BACKGROUND_COLOR = "#B1DDC6"
 
+window = Tk()
+window.title('Flashy')
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+# The canvas defined to image size of 800x526, it will be when 'columnspan=2' become nicely arrange
+# to grid rest of the component
+canvas = Canvas(width=800, height=526, highlightthickness=0, bg=BACKGROUND_COLOR)
+card_front_img = PhotoImage(file="images/card_front.png")
+canvas.create_image(400, 263, image=card_front_img)
+canvas.create_text(400, 150, text="Title", font=("Ariel", 35, "italic"))
+canvas.create_text(400, 263, text="word", font=("Ariel", 50, "bold"))
+canvas.grid(row=0, column=0, columnspan=2)
+
+cross_image = PhotoImage(file="images/wrong.png")
+unknown_button = Button(image=cross_image, highlightthickness=0, bd=0)
+unknown_button.grid(row=1, column=0)
+
+check_image = PhotoImage(file="images/right.png")
+known_button = Button(image=check_image, highlightthickness=0, bd=0)
+known_button.grid(row=1, column=1)
+
+window.mainloop()
