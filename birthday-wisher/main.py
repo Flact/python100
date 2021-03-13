af834f3545e909192c69ffeb0af9dc3dd79659e9
Anushka Milinda <anushkamilinda9@gmail.com>
2021-03-13 00:44:12 -0500

    Completed Automated Birthday Wisher

---
+++ birthday-wisher/main.py
@@ -0,0 +1,24 @@
+import random
+import smtplib
+from datetime import datetime as dt
+
+import pandas as pd
+
+g_mail = "sumududalamal6@gmail.com"
+g_pw = "xhBCB9*gi%CZ@*"
+
+today = [dt.now().month, dt.now().day]
+
+data = pd.read_csv("birthdays.csv")
+row = data[(data['month'] == today[0]) & (data['day'] == today[1])]  # Very Very important: if Selecting rows,
+# based on multiple column conditions, wrap those condition with brackets
+
+if not row.empty:
+    with open(f"letter_templates/letter_{random.randint(1, 3)}.txt", "r") as f:
+        msg = f.read().replace('[NAME]', row["name"].item())
+        print(msg)
+        with smtplib.SMTP("smtp.gmail.com", 587) as conn:
+            conn.starttls()
+            conn.login(user=g_mail, password=g_pw)
+            conn.sendmail(from_addr=g_mail, to_addrs=row["email"].item(),
+                          msg=f"Subject:Happy Birthday\n\n{msg}")
