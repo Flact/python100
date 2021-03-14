8c08bffc917f04631f4ad19292412dc2e8d459c9
Anushka Milinda <anushkamilinda9@gmail.com>
2021-03-13 22:38:49 -0500

    Understand API Parameters Match Sunset Times with the Current Time

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
 import requests
+from datetime import datetime
 
-response = requests.get("http://api.open-notify.org/iss-now.json")
-# print(response.content)
-# print(type(response.content))
+# response = requests.get("http://api.open-notify.org/iss-now.json")
+# # print(response.content)
+# # print(type(response.content))
+#
+# response.raise_for_status()
+#
+# data = response.json()
+# longitude = data["iss_position"]["longitude"]
+# latitude = data["iss_position"]["latitude"]
+# iss_position = (longitude, latitude)
+# print(iss_position)
 
+parameters = {
+    "lat": 40.741895,
+    "lng": -73.989308,
+    "formatted": 0,
+
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
-
 data = response.json()
-longitude = data["iss_position"]["longitude"]
-latitude = data["iss_position"]["latitude"]
-iss_position = (longitude, latitude)
-print(iss_position)
+
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
+
+time_now = datetime.now()
+
+print(sunrise)
+print(sunset)
+print(time_now.hour)
