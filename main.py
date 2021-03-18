6ae4495b940794ed0918eb158741f54cb041cac2
Anushka Milinda <anushkamilinda9@gmail.com>
2021-03-18 12:42:59 -0400

    Using API Keys to Authenticate and Get the Weather from OpenWeatherMap

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
+import json
+
 import requests
 
-API_KEY = "cca441dbe68d12bf238a4e7c18e3ec22"
-LAT = 36.778259
-LON = -119.417931
-
-parameters = {
-    "lat": LAT,
-    "lon": LON,
-    "appid": API_KEY
-}
+with open('weather_params.json') as f:
+    parameters = json.load(f)
 
 response = requests.get("https://api.openweathermap.org/data/2.5/onecall", params=parameters)
 
