6243d59be329fa7c16e37a5a828b64b5361b24f8
Anushka Milinda <anushkamilinda9@gmail.com>
2021-03-19 19:11:29 -0400

    Check if it Will Rain in the Next 12 Hours

--- main.py
+++ main.py
@@ -6,5 +6,11 @@ with open('weather_params.json') as f:
     parameters = json.load(f)
 
 response = requests.get("https://api.openweathermap.org/data/2.5/onecall", params=parameters)
+response.raise_for_status()
 
-print(response.json())
+data = response.json()["hourly"]
+conditions = [sub_data["weather"][0]["id"] for sub_data in data[:12] if sub_data["weather"][0]["id"] < 700]
+
+print(len(conditions))
+print(conditions)
+
