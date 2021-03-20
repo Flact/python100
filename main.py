512e49daa5277b62f38c205bc40d251ada43c9e1
Anushka Milinda <anushkamilinda9@gmail.com>
2021-03-20 18:00:21 -0400

    Add a Pixel to the Habit Tracker using a Post Request and Autofilling today's date using strftime

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
-import requests
 import json
 from datetime import date
+
+import requests
 
 with open("day_37.json") as f:
     data = json.load(f)
 
 graph_url = f"{data['endpoint']}/{data['user_params']['username']}/graphs"
 
-pixel_url = f"{data['endpoint']}/{data['user_params']['username']}/graphs/{data['graph_config']['id']}"
+pixel_url = f"{graph_url}/{data['graph_config']['id']}"
 
 today = date.today().strftime("%Y%m%d")
 
 pixel_param = {
     "date": f"{today}",
-    "quantity": "5.50"
+    "quantity": "15.50"
 }
 
-# print(type(today))
+update_pixel_param = {
+    "quantity": "15.50"
+}
+pixel_update_url = f"{pixel_url}/20210319"
 
-res = requests.post(url=pixel_url, json=pixel_param, headers=data['headers'])
+# res = requests.post(url=pixel_url, json=pixel_param, headers=data['headers'])
+res = requests.put(url=pixel_update_url, json=update_pixel_param, headers=data['headers'])
 print(res.text)
