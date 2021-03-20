ecd232bbd881a4db51844cd9c8cf259fc6f983cd
Anushka Milinda <anushkamilinda9@gmail.com>
2021-03-20 15:37:34 -0400

    Get the News Articles and Print MSG

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import json
-from datetime import date, timedelta
+from itertools import islice
 
 import requests
 
 COMPANY_NAME = "Tesla Inc"
 
 
-def pcent_diff(num1: float, num2: float) -> float:
-    return float('{0:.2f}'.format(abs((num1 - num2) / (num1 + num2) * 100)))
+def pcent_diff(yst: float, d_bf_yst: float) -> float:
+    return float('{0:.2f}'.format(abs(((yst - d_bf_yst) / d_bf_yst) * 100)))
 
 
 with open("day_36.json") as f:
@@ -17,28 +17,22 @@ response = requests.get(data["alphavantage"]["url"])
 response.raise_for_status()
 stock_data = response.json()["Time Series (Daily)"]
 
-today = date.today()
-y_day1 = str(today - timedelta(days=1))
-y_day2 = str(today - timedelta(days=2))
+list_data = [[key, value["4. close"]] for (key, value) in islice(stock_data.items(), None, 2)]
 
-n1 = float(stock_data[y_day1]["4. close"])
-n2 = float(stock_data[y_day2]["4. close"])
+diff = pcent_diff(float(list_data[0][1]), float(list_data[1][1]))
 
-diff = pcent_diff(n1, n2)
+print(diff)
 
 if diff > 0:
-    print("Get News")
+    url = data["newsapi"]["url"]
+    parameters = {
+        "qInTitle": COMPANY_NAME,
+        "sortBy": "popularity",
+        "apiKey": data["newsapi"]["apiKey"]}
 
-url = data["newsapi"]["url"]
-parameters = {
-    "q": COMPANY_NAME,
-    "from": f"{y_day1}",
-    "sortBy": "popularity",
-    "apiKey": data["newsapi"]["apiKey"]}
+    res = requests.get(url, params=parameters)
+    res_json = res.json()
 
-res = requests.get(url, params=parameters)
-res_json = res.json()
+    msg = [m["content"] for m in res_json["articles"][:3]]
 
-msg = [m["content"] for m in res_json["articles"][:3]]
-
-print(msg)
+    print(msg)
